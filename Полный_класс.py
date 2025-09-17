022a91f3e827c9e964d3134b363a9e94b0100e14
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2025-09-17 10:53:27 +0300

    4. Сделать методы класса (статические) валидации всех необходимых полей.

--- Полный_класс.py
+++ Полный_класс.py
@@ -1,3 +1,4 @@
+import re
 class Client:
     def __init__(self):
         self.__client_id = None
@@ -6,6 +7,26 @@ class Client:
         self.__otch = None
         self.__address = None
         self.__phone = None
+
+    @staticmethod
+    def is_valid_id(id):
+        return id is not None and id > 0
+
+    @staticmethod
+    def is_valid_name(name):
+        return name is not None and re.match(r'^[А-Яа-яЁё]+$', name) is not None
+
+    @staticmethod
+    def is_valid_otch(otch):
+        return otch is None or otch == "" or re.match(r'^[А-Яа-яЁё]+$', otch) is not None
+
+    @staticmethod
+    def is_valid_phone(phone):
+        return phone is not None and re.match(r'^\+\d{11}$', phone) is not None
+
+    @staticmethod
+    def is_valid_address(address):
+        return address is not None and address.strip() != ""
 
     @property
     def client_id(self):
@@ -34,16 +55,33 @@ class Client:
 
 
     def set_client_id(self, client_id):
+        if not Client.is_valid_id(client_id):
+            raise ValueError("ID клиента должно быть положительным числом")
         self.__client_id = client_id
+
     def set_last_name(self, last_name):
+        if not Client.is_valid_name(last_name):
+            raise ValueError("Фамилия должна содержать только буквы и не буть пустой")
         self.__last_name = last_name
+
     def set_first_name(self, first_name):
+        if not Client.is_valid_name(first_name):
+            raise ValueError("Имя должно содержать только буквы и не быть пустым")
         self.__first_name = first_name
+
     def set_otch(self, otch):
+        if not Client.is_valid_otch(otch):
+            raise ValueError("Отчество должно содержать только буквы")
         self.__otch = otch
+
     def set_address(self, address):
+        if not self.is_valid_address(address):
+            raise ValueError("Адрес не должен быть пустым")
         self.__address = address
+
     def set_phone(self, phone):
+        if not Client.is_valid_phone(phone):
+            raise ValueError("Номер телефона должен начинаться с '+' и содержать 11 цифр, а также не быть пустым")
         self.__phone = phone
 
 client = Client()
