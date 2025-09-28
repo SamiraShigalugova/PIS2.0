bfc21e132aaa8a9ec5672b6f6c7975184618e112
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2025-09-28 11:57:31 +0300

    b. get_k_n_short_list Получить список k по счету n объектов класса short

--- client_db.py
+++ client_db.py
@@ -58,6 +58,33 @@ class ClientRepDB:
             )
         return None
 
+    def get_k_n_short_list(self, k, n):
+
+        offset = (n - 1) * k
+        query = """
+         SELECT client_id, last_name, first_name, phone, address 
+         FROM clients 
+         ORDER BY client_id 
+         LIMIT %s OFFSET %s
+         """
+        result = self.execute_query(query, (k, offset), fetch=True)
+
+        short_clients = []
+        for row in result:
+            short_client = Client(
+                client_id=row[0],
+                last_name=row[1],
+                first_name=row[2],
+                phone=row[3],
+                address=row[4]
+            )
+            short_clients.append(short_client.short())
+
+        return short_clients
+
+
+
+
 repo_db = ClientRepDB(
     host="localhost",
     user="postgres",
@@ -69,3 +96,7 @@ print("\nПоиск клиента по ID:")
 client = repo_db.get_by_id(2)
 if client:
     print(f"Найден клиент: {client.get_long_info()}")
+print("\nВыборка клиентов:")
+short_clients = repo_db.get_k_n_short_list(k=3, n=1)
+for i, short_client in enumerate(short_clients, 1):
+    print(f"{i}. {short_client.get_info()}")
